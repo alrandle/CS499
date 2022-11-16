9160eb76a19aad2f850a9167b3a6f00ce1084a46
Alex Randle <55765244+alrandle@users.noreply.github.com>
2022-11-16 14:11:00 -0500

    Add files via upload
    The start of the teacher file. Just a small intro to the file for now

---
+++ src/pages/teacher.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Teacher(){
+    return (
+        <div className='text-center text-white'>
+          <h1>Teacher Welcome Page</h1>
+        </div>
+    )
+}
+
+export default Teacher;
