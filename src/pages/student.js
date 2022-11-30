4a55cb0ac467657584e4e9c75503017f0cdecdd8
Alex Randle <alexrrandle2001@gmail.com>
2022-11-30 15:04:55 -0500

    Running changes

---
+++ src/pages/student.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Student(){
+    return (
+        <div className='text-center text-white'>
+          <h1>testing student page</h1>
+        </div>
+    )
+}
+
+export default Student
