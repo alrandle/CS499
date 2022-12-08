f2984f683a0ad3824be8d0ffd5a165a68f0be637
Alex Randle <alexrrandle2001@gmail.com>
2022-12-08 16:08:47 -0500

    first changes to analytics

--- src/pages/analytics.js
+++ src/pages/analytics.js
@@ -5,9 +5,34 @@
  * Daniel
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
+let loaded = false
+async function selectQuiz(){
+  //have options for the quizes?
+  if(loaded == false){
+    const tempQuizes = [
+      "Quiz 1",
+      "Quiz 2",
+    ]
+    let messageLocation = document.getElementById("current-quiz")
+    let currentMessage = messageLocation.innerHTML
+    for(let i = 0; i < tempQuizes.length; i++){
+      let message = `<button id=${tempQuizes} type="button" onClick={}>${tempQuizes[i]}</button>`
+      currentMessage = currentMessage + message
+      messageLocation.innerHTML = currentMessage
+
+    }
+    loaded = true
+  }
+}
+
+async function loadGrades(){
+  console.log("made it")
+  
+}
 
 class Analytics extends React.Component{
     state = {
@@ -34,6 +59,17 @@ class Analytics extends React.Component{
   render(){
     return(
       <div className='text-center text-white'>
+        <h1>Quiz analytics</h1>
+        <br></br>
+        <div className='text-center text-white'>
+          <h3>Please Select A Quiz From Below To Grade</h3>
+          <button onClick={loadGrades}>Click To Load Grades</button>
+        </div>
+        <br></br>
+        <div className='text-center text-white'>
+          <h3>Here are your individual student grades</h3>
+          <div id='student-grades'></div>
+        </div>
       </div>
     )
   }
